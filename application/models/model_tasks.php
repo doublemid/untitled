0becc5b9289ff312ce9b56d7ffa284a4bf2532d8
dd <dd>
2020-09-08 23:14:49 +0600

    edit task

--- application/models/model_tasks.php
+++ application/models/model_tasks.php
@@ -1,23 +1,5 @@
 <?php
-/*class Model {
-    public $pdo;
-    public $data;
-    function __construct(){
-        $host = '127.0.0.1';
-        $db   = 'tasks';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8';
 
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-        $this->pdo = new PDO($dsn, $user, $pass, $opt);
-    }
-}*/
 
 class Model_Tasks extends Model{
     protected $num=3;
@@ -98,10 +80,3 @@ class Model_Tasks extends Model{
         return $result;
     }
 }
-/*$data['id'] = 3;
-$data['status'] = 0;
-$data['text'] ='fsdfsdf';
-print_r( $data);
-$fffd = new Model_Tasks();
-$result = $fffd->editTask($data);
-print_r($result);*/
